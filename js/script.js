7e731403bf3ece130d8d190165e50d49e7ecd4c0
bp <bp@7bp.de>
2016-02-28 17:18:38 +0100

    Add website

---
+++ js/script.js
@@ -0,0 +1,35 @@
+(function($) {
+  "use strict";
+
+  $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 60
+  });
+
+  new WOW().init();
+  
+  $('#collapsingNavbar li a').click(function() {
+      /* always close responsive nav after click */
+      $('.navbar-toggler:visible').click();
+  });
+
+  var imvuboxQueryTimer = null;
+  window.imvuboxQuery = function(input) {
+    $('.form-inline .spinner').show();
+    clearTimeout(imvuboxQueryTimer);
+    imvuboxQueryTimer = setTimeout(function() {
+      location.hash = '#/'+input.value;
+      $('.list-group a').addClass('hidden');
+      $('.list-group a').each(function(x) {
+        var item = $(this);
+        if (item.data('search').indexOf(input.value) !== -1) {
+          item.removeClass('hidden');
+        } else {
+          item.addClass('hidden');
+        }
+      });
+      $('.form-inline .spinner').hide();
+    }, 500);
+  };
+
+})(jQuery);
